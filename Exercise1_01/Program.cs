625f28a2ac444c64d16fe8dff695881765e38803
atemengue <moafembe@gmail.com>
2023-12-18 04:30:42 +0100

    init: getting started on learning csharp programming

---
+++ Exercise1_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 0;
+int b = 0;
+
+int c, d = 100;
+
+var name = "Elon Musk";
+
+Console.WriteLine(name);
