373ce51935149376d0f5e5ceb8e02dbbf0feafe8
atemengue <moafembe@gmail.com>
2023-12-18 04:38:41 +0100

    Exercice 02

---
+++ Exercise1_02/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("It's your first program, What is your name? ");
+var name = Console.ReadLine();
+
+Console.WriteLine($" Hi {name}, it is very nice to meet  you, Let's write code together");
