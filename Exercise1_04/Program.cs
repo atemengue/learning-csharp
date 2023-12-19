39584f9aa0829620f5763a51f2e62cfeb7fc5d91
atemengue <moafembe@gmail.com>
2023-12-19 03:07:12 +0100

    Exercice 04

---
+++ Exercise1_04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Type a value for a: ");
+var a = int.Parse(Console.ReadLine());
+Console.WriteLine("Type a value for b:");
+var b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The value of a is  {a} and for b is {b}");
+Console.WriteLine($"Sum is {a + b}");
+Console.WriteLine($"Multipliction is {a * b}");
+Console.WriteLine($"Subtraction is {a - b}");
+Console.WriteLine($"Division is {a / b}");
