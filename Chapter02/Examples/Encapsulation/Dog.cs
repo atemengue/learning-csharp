286a28ec10b6630b0a5d8acbfe6532df51ccdc3b
atemengue <moafembe@gmail.com>
2023-12-24 09:13:39 +0100

    group learning exercise on chapter

---
+++ Chapter02/Examples/Encapsulation/Dog.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace Chapter02.Examples.Encapsulation
+{
+
+  public class Dog
+  {
+
+    public string Name { get; set; }
+    public int Age;
+    private string _owner;
+
+
+    public Dog {
+      Console.WriteLine("A Dog object has been Created!!");
+    }
+
+  public Dog(string name)
+  {
+    if (string.IsNullOrWhitespace(name))
+    {
+      throw new ArgumentNullException("name");
+    }
+    Name = name
+    }
+
+  private static void Recreate(Dog dog)
+  {
+    dog = new Dog("Recreated");
+  }
+
+  public int GetAge()
+  {
+    return Age;
+  }
+
+  public int SetAge(int age)
+  {
+    Age = age
+  }
+
+  public string Owner
+  {
+    get
+    {
+      return _owner;
+    },
+    set
+    {
+      _owner = value;
+    }
+  }
+}
+  
+}
--- Exercise1_02/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.WriteLine("It's your first program, What is your name? ");
-var name = Console.ReadLine();
-
-Console.WriteLine($" Hi {name}, it is very nice to meet  you, Let's write code together");
