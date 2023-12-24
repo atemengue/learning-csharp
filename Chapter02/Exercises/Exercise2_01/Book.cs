b0ea219712d498b1cfc60bc9d48130b23f7e8e6c
atemengue <moafembe@gmail.com>
2023-12-24 09:10:05 +0100

    started chapter02 with exercices

---
+++ Chapter02/Exercises/Exercise2_01/Book.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Chapter02.Exercise.Exercise2_01
+{
+  public class Book
+  {
+    public string Title;
+    public string Author;
+    public string Publisher;
+    public int Pages;
+    public string Description;
+  }
+}
---
+++ Chapter02/Exercises/Exercise2_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using Chapter02.Exercise.Exercise2_01;
+
+public static class Solution
+{
+  public static void Main()
+  {
+    Book book1 = new Book();
+    book1.Author = "New Writer";
+    book1.Title = "First books";
+    book1.Publisher = "Publisher 1";
+
+    Book book2 = new Book();
+    book2.Author = "New Writer";
+    book2.Title = "Second Book";
+    book2.Publisher = "Publisher 2";
+    book2.Description = "Interesting read";
+
+    Print(book1);
+    Print(book2);
+
+  }
+
+  public static void Print(Book book)
+  {
+    Console.WriteLine($"Author: {book.Author}, " +
+                      $"Title: {book.Title}, " +
+                      $"Publisher: {book.Publisher}, " +
+                      $"Description: {book.Description}. ");
+  }
+
+}
