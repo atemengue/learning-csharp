cf66f4946da380cf8ab25161a16d221fdf129164
atemengue <moafembe@gmail.com>
2023-12-24 09:09:21 +0100

    group exercise on chapter

---
+++ Chapter01/Exercise1_04/Program.cs
@@ -0,0 +1,27 @@
+﻿using Exerice_04;
+// Console.WriteLine("Type a value for a: ");
+// var a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Type a value for b:");
+// var b = int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"The value of a is  {a} and for b is {b}");
+// Console.WriteLine($"Sum is {a + b}");
+// Console.WriteLine($"Multipliction is {a * b}");
+// Console.WriteLine($"Subtraction is {a - b}");
+// Console.WriteLine($"Division is {a / b}");
+
+
+
+var person = new Person("Regis", 29);
+
+var person2 = new Person();
+
+var person3 = new Person()
+{
+  Name = "Fred",
+  Age = 9
+};
+
+person.GetInfo();
+
+person3.GetInfo();
---
+++ Chapter01/Exercise1_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Exercice1_11
+{
+
+  public class Program
+  {
+    public static async Task Main()
+    {
+      using (var fileStream = new FileStream("Exercise1_11/text.csv", FileMode.Open, FileAccess.Read))
+      {
+        using (var reader = new StreamReader(fileStream))
+        {
+          var content = await reader.ReadToEndAsync();
+          var lines = content.Split(Environment.NewLine);
+          foreach (var line in lines)
+          {
+            Console.WriteLine(line);
+          }
+        }
+      }
+    }
+  }
+
+}
