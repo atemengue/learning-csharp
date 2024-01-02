91e388c7e54e988ca86142e85fc1acd0bbfcbb16
atemengue <moafembe@gmail.com>
2024-01-02 05:11:48 +0100

    started delegates

---
+++ Chapter03/delegates/example01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public static class Solution
+{
+
+  public class Point
+  {
+
+    private double X { get; set; }
+    private double Y { get; set; }
+  }
+
+  public interface ITransform
+  {
+    Point Move(double height, double width);
+  }
+
+  public class RotateTransform : ITransform
+  {
+    public Point Move(double height, double width)
+    {
+      return new Point();
+    }
+
+  }
+
+  public class ZoomTransform : ITransform
+  {
+    public Point Move(double height, double width)
+    {
+      return new Point;
+    }
+  }
+
+
+  public class Transform
+  {
+    public void Transform()
+    {
+      var rotatePoint = Calculate(new RotateTransform(), 100, 20);
+      var zoomPoint = Calculate(new ZoomTransform(), 5, 5);
+    }
+
+    public void Calculate(ITransform transform, double height, double width)
+    {
+      var point = transform.Move(height, width);
+      return point;
+    }
+  }
+
+  public static void Main()
+  {
+
+    Console.WriteLine("Hello");
+  }
+}
