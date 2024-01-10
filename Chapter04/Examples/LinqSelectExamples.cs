0ce6483f29d9599b20ccfb533415448ef989dcf2
atemengue <moafembe@gmail.com>
2024-01-10 06:58:36 +0100

    Chapter04 Exercise04 Linq Selectors: Mixes operators and query Expressions

--- Chapter04/Examples/LinqSelectExamples.cs
+++ Chapter04/Examples/LinqSelectExamples.cs
@@ -12,10 +12,37 @@ namespace Chapter04.Examples {
       foreach(var day in query1)
         Console.WriteLine($"Query1: {day}");
 
+      // Linq Query Operators
+
+      var query4 = from day in days
+                   select day + "day";
+      
+      foreach (var day in query4)
+        Console.WriteLine($"Query Operartor: {day}");
+                
+
       var query2 = days.Select((d, i) => $"{i} : {d}day");
       foreach(var day in query2)
         Console.WriteLine($"Query2: {day}");
 
+      // ANONYMOUS TYPES
+      var query3 = days.Select((d, i) => new {
+        Index = i,
+        UpperCaseName = $"{d.ToUpper()}DAY"
+      });
+      foreach (var day in query3)
+        Console.WriteLine($"Query3: Index={day.Index}, UpperCaseDay={day.UpperCaseName}");
+
+      // Mixes a Query Operator and Query Expressions
+
+      var query5 = from dayIndex in days.Select((d, i) => new {Name = d, Index = i})
+                   select dayIndex;
+                    
+      foreach (var day in query5)
+        Console.WriteLine($"Query5: Index={day.Index} : {day.Name}");
+
+    
+      Console.ReadLine();
     }
   }
 }
